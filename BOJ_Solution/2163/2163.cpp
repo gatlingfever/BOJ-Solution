ea791c944ab27d3cd873c550ce1871bceebf0b4d
동명 안 <gatlingfever34@gmail.com>
2017-04-26 16:05:03 +0900

    2163 complete

---
+++ BOJ_Solution/2163/2163.cpp
@@ -0,0 +1,16 @@
+// Question : https://www.acmicpc.net/problem/2163
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nN = 0;
+	int nM = 0;
+	cin >> nN >> nM;
+	cout << nN * nM - 1;
+	
+	system("pause");
+
+	return 0;
+}
